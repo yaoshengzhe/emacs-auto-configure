8c066b5dc7d40dcbffc5bfd3707064b1ecb19a3c
yao <yaoshengzhe@gmail.com>
2012-03-05 00:04:21 -0800

    load guy specific libraries as necessary

--- .emacs.d/config.el
+++ .emacs.d/config.el
@@ -3,17 +3,13 @@
 ;;(load-library "layout-restore/config.el")
 ;;(load-library "muse/config.el")
 
-
-
 (if window-system
-    (load-library "color-theme/config.el"))
-
-(if window-system
-    (load-library "tabbar/config.el"))
+    (progn
+      (load-library "tabbar/config.el")
+      (load-library "color-theme/config.el")))
 
 (load-library "basic/config.el")
 (load-library "yasnippet/config.el")
-(load-library "color-theme/config.el")
 (load-library "auto-complete/config.el")
 (load-library "template/config.el")
 (load-library "rainbow-mode/config.el")
