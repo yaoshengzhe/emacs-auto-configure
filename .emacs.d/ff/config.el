4532fc69242932e097f8700dae0bbbd95aadc617
Shengzhe Yao <syao@quantcast.com>
2012-08-17 22:58:48 -0700

    rename haskell-mode to ff

---
+++ .emacs.d/ff/config.el
@@ -0,0 +1,8 @@
+(add-to-list 'load-path "~/.emacs.d/haskell-mode")
+(load "haskell-mode/haskell-site-file")
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+
+(yas/load-directory yas/my-directory)
