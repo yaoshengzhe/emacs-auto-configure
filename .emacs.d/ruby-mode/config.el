68025d35413fcebeb234e31f0dcde93cce798c9e
Shengzhe Yao <syao@quantcast.com>
2012-08-21 17:02:09 -0700

    modify ruby-mode config.el file

--- .emacs.d/ruby-mode/config.el
+++ .emacs.d/ruby-mode/config.el
@@ -1,10 +1,15 @@
 (add-to-list 'load-path "~/.emacs.d/ruby-mode")
 
+(load "ruby-mode/ruby-mode")
+
+
 ;; Load ruby mode when needed
 (autoload 'ruby-mode "ruby-mode" "Ruby mode" t )
 ;; Assign .rb and .rake files to use ruby mode
 (setq auto-mode-alist (cons '("\\.rb\\'" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake\\'" . ruby-mode) auto-mode-alist))
+
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 ;; Show syntax highlighting when in ruby mode
 (add-hook 'ruby-mode-hook '(lambda () (font-lock-mode 1)))
 
