725803c44de398dc90a1eb803c5f4776259bf3a4
Shengzhe Yao <syao@rocketfuel.com>
2013-10-03 14:07:33 -0700

    open Vagrantfile in ruby mode

--- .emacs.d/ruby-mode/config.el
+++ .emacs.d/ruby-mode/config.el
@@ -2,12 +2,12 @@
 
 (load "ruby-mode/ruby-mode")
 
-
 ;; Load ruby mode when needed
 (autoload 'ruby-mode "ruby-mode" "Ruby mode" t )
 ;; Assign .rb and .rake files to use ruby mode
 (setq auto-mode-alist (cons '("\\.rb\\'" . ruby-mode) auto-mode-alist))
 (setq auto-mode-alist (cons '("\\.rake\\'" . ruby-mode) auto-mode-alist))
+(setq auto-mode-alist (cons '("Vagrantfile\\'" . ruby-mode) auto-mode-alist))
 
 (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
 ;; Show syntax highlighting when in ruby mode
