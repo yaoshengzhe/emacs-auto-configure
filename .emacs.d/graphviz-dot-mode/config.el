2b52b383f296b4aad23645e4008bd248d512f93f
Shengzhe Yao <syao@rocketfuel.com>
2014-05-15 16:15:46 -0700

    fix indent and semicolon issue in graphviz-mode
    1. set indent width to two spaces
    2. don't start new line when semi colon is entered

--- .emacs.d/graphviz-dot-mode/config.el
+++ .emacs.d/graphviz-dot-mode/config.el
@@ -1,5 +1,9 @@
 (add-to-list 'load-path "~/.emacs.d/graphviz-dot-mode")
 
+;; set indent width to two spaces
 (setq graphviz-dot-indent-width 2)
+
+;; don't start new line when semi colon is entered
+(setq graphviz-dot-auto-indent-on-semi nil)
 
 (load-library "graphviz-dot-mode/graphviz-dot-mode-autoloads.el")
