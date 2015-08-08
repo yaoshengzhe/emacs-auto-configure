0cc52ca85fe3940b45464ab0bb3d65b0a529103b
Shengzhe Yao <yaoshengzhe@gmail.com>
2015-08-08 00:13:45 -0700

    fix go oracle install

--- install.sh
+++ install.sh
@@ -20,5 +20,6 @@ mv bin/* /usr/local/bin
 rm -rf bin
 
 if [ -f `which go` ]; then
-   go get golang.org/x/tools/cmd/oracle
+    go get golang.org/x/tools/cmd/oracle
+    mv $GOPATH/bin/oracle $GOROOT/bin/
 fi
